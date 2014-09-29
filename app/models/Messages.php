b31db837524d902dcbd4bec9119f6029f62924b5
Nicky Semenza <14nicholasse@gmail.com>
2014-09-29 09:44:32 -0400

    table renaming

--- app/models/Messages.php
+++ app/models/Messages.php
@@ -1,5 +1,6 @@
 <?php
 class Messages extends Eloquent{
     protected $fillable = array('from_id','from_name','thread_id','time','message');
+    protected $table = 'chat_messages';
 
 }
--- app/models/Threads.php
+++ app/models/Threads.php
@@ -2,5 +2,6 @@
 class Threads extends Eloquent{
     protected $fillable = array('thread_id','message_count','participants_names','participants_ids'); // need to read up on mass assignment/ security issues
     protected  $primaryKey='thread_id';
+    protected $table = 'chat_threads';
 
 }
